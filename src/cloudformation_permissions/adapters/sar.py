ad3aaa002ec2426e1e1b03a5152cea971cb1b12a
George Alton <galton@evertz.com>
2025-06-05 14:41:50 +0100

    Include a version of the SAR data in pkg

--- src/cloudformation_permissions/adapters/sar.py
+++ src/cloudformation_permissions/adapters/sar.py
@@ -1,11 +1,13 @@
 import fnmatch
 import json
-import pathlib
+
+from importlib import resources
 from functools import cache, cached_property
 from types import MappingProxyType
 from typing import Literal, Mapping, Protocol, Sequence, TypedDict
 
-type AccessLevel = Literal["List", "Read", "Write", "Permissions Management", "Tagging"]
+type AccessLevel = Literal["List", "Read",
+                           "Write", "Permissions Management", "Tagging"]
 
 
 class ActionResourceType(TypedDict):
@@ -60,13 +62,16 @@ type ServiceAuthorizationReference = list[Service]
 
 
 class ServiceAuthorizationReferenceProcotol(Protocol):
-    def list_actions_by_pattern(self, pattern: ActionPattern) -> Sequence[QualifiedAction]: ...
+    def list_actions_by_pattern(
+        self, pattern: ActionPattern) -> Sequence[QualifiedAction]: ...
 
 
 class ServiceAuthorizationReferenceLocal(ServiceAuthorizationReferenceProcotol):
     def __init__(self):
-        path = pathlib.Path("/tmp/auth.json")
-        self.index: ServiceAuthorizationReference = json.loads(path.read_text())
+        path = resources.files("cloudformation_permissions").joinpath(
+            "data", "auth.json")
+        self.index: ServiceAuthorizationReference = json.loads(
+            path.read_text())
         self.list_actions_by_pattern = cache(self._list_actions_by_pattern)
 
     @cached_property
@@ -75,7 +80,8 @@ class ServiceAuthorizationReferenceLocal(ServiceAuthorizationReferenceProcotol):
         for service in self.index:
             for action in service["actions"]:
                 _name = f"{service['servicePrefix']}:{action['name']}"
-                _actions[_name] = QualifiedAction(**action, qualifiedName=_name)
+                _actions[_name] = QualifiedAction(
+                    **action, qualifiedName=_name)
         return MappingProxyType(_actions)
 
     def _list_actions_by_pattern(self, pattern: ActionPattern) -> list[QualifiedAction]:
