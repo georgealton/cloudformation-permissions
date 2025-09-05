6af14ac2830968045003541be67e976838e9e17e
George Alton <georgealton@gmail.com>
2025-09-05 09:27:08 +0100

    Update test_sts.py

--- tests/integration/test_sts.py
+++ tests/integration/test_sts.py
@@ -1,3 +1,5 @@
+import re
+
 from boto3 import client
 from cloudformation_permissions.adapters import sts
 
@@ -5,5 +7,5 @@ from cloudformation_permissions.adapters import sts
 def test_sts():
     c = client("sts")
     s = sts.STS(c)
-    role = "arn:aws:iam::737710810646:role/AWSReservedSSO_EvertzIOAdmin_e2e5cb6ac866872e"
-    assert str(s()) == role
+    role_pattern = "arn:aws:iam::.*:role/.*"
+    assert re.fullmatch(role_pattern, str(s())) is not None
